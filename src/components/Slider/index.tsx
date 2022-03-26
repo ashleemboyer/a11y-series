d39874294055dbde396b645ad74ddfd01181f4d4
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2022-03-26 15:37:41 -0400

    Cleaning up Slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
 import keyCodes from "../../constants/keyCodes";
-import styles from "./Slider.module.scss";
+import s from "./slider.module.css";
+
+const THUMB_SIZE = 24;
 
 const Slider = ({
-  minValue,
-  maxValue,
-  stepSize,
-  initialValue,
-  onChangeCallback,
-  label,
   ariaLabel,
+  initialValue,
+  label,
+  maxValue,
+  minValue,
+  onChangeCallback,
+  stepSize,
 }) => {
   const [currentValue, setCurrentValue] = useState(initialValue || minValue);
   const currentValuePercentage = (currentValue / (maxValue - minValue)) * 100;
@@ -47,45 +48,31 @@ const Slider = ({
   };
 
   return (
-    <div className={styles.SliderContainer}>
+    <div className={s.root}>
       {label && (
-        <div id="slider-label" className={styles.SliderLabel}>
+        <div id="slider-label" className={s.SliderLabel}>
           {label}
         </div>
       )}
-      <div className={styles.Slider}>
-        <div className={styles.SliderRail}></div>
+      <div className={s.Slider}>
+        <div className={s.SliderRail}></div>
         <div
           role="slider"
-          tabIndex="0"
+          tabIndex={0}
           aria-valuemin={minValue}
           aria-valuenow={currentValue}
           aria-valuemax={maxValue}
           aria-labelledby={label ? "slider-label" : undefined}
           aria-label={label ? undefined : ariaLabel}
-          className={styles.SliderThumb}
+          className={s.SliderThumb}
           style={{
-            left: `calc(${currentValuePercentage}% - ${styles.thumbSize} / 2)`,
+            left: `calc(${currentValuePercentage}% - ${THUMB_SIZE}px / 2)`,
           }}
           onKeyDown={handleKeyDown}
         ></div>
       </div>
     </div>
   );
-};
-
-Slider.propTypes = {
-  minValue: PropTypes.number.isRequired,
-  maxValue: PropTypes.number.isRequired,
-  stepSize: PropTypes.number.isRequired,
-  initialValue: PropTypes.number,
-  onChangeCallback: PropTypes.func,
-  label: PropTypes.string,
-  ariaLabel: PropTypes.string,
-};
-
-Slider.defaultProps = {
-  onChangeCallback: () => {},
 };
 
 export default Slider;
