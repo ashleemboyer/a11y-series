6abe979fffc01bb0195067ba5a930cc0301a49e7
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2022-03-26 14:10:45 -0400

    Adding 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <h1>404</h1>
+      <Link href="/">Go back home</Link>
+    </>
+  );
+};
+
+export default NotFoundPage;
