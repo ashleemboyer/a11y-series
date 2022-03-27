31cdd46db02fa90210cb3e0d751c505de3798d8a
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2022-03-27 09:59:22 -0400

    Fixing fontawesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import type { AppProps, NextWebVitalsMetric } from "next/app";
 import Link from "next/link";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, fas);
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
