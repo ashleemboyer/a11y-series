bb8812a4d93d15e6f8b70a439c30e8ef7d246aec
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2022-03-27 13:56:25 -0400

    Removing web vitals catcher

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,17 +19,4 @@ const App = ({ Component, pageProps }: AppProps) => {
   );
 };
 
-const abbreviationsToNames = {
-  TTFB: "Time to First Byte",
-  FCP: "First Contentful Paint",
-  LCP: "Largest Contentful Paint",
-  FID: "First Input Delay",
-  CLS: "Cumulative Layout Shift",
-};
-
-export function reportWebVitals(metric: NextWebVitalsMetric) {
-  const { id, name, startTime, value, label } = metric;
-  console.log(metric);
-}
-
 export default App;
